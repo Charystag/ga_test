69ccda0234a87a8a630043f66f3ec8418d61ef34
Shelltief <shelltief@proton.me>
2025-05-12 15:17:15 +0200

    Added java project

---
+++ java/my-app/src/main/java/com/mycompany/app/Calculatrice.java
@@ -0,0 +1,8 @@
+package com.mycompany.app; // Assure-toi que le package est correct
+
+public class Calculatrice {
+    public int additionner(int a, int b) {
+        // Laisser "cassé" ou non implémenté pour l'instant
+	return (a + b);
+    }
+}
